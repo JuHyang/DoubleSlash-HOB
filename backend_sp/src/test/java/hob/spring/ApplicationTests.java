d9bbce497507deed0494f058f4fe61779e7c0865
이민규(***5***054) <zx6486@kau.kr>
2020-07-21 21:27:31 +0900

    Generate spring boot project

---
+++ backend_sp/src/test/java/hob/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package hob.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
